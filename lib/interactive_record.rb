4aea6fe22673dd09d0c532d8c21af42e01c036e8
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-28 17:57:28 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,6 +48,11 @@ class InteractiveRecord
   end
 
   def self.find_by_name(name)
-
+    sql = <<-SQL
+      SELECT *
+      FROM ?
+      WHERE name = ?
+    SQL
+    DB[:conn].execute(sql, table_name_for_insert, name)
   end
 end
