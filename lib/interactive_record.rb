9e2c9940b3a2a515b2f3778be02704389d87533f
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-28 17:33:46 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -28,4 +28,8 @@ class InteractiveRecord
   def table_name_for_insert
     self.class.table_name
   end
+
+  def column_names_for_insert
+    self.class.column_names.delete_if{|col| col == 'if'}
+  end
 end
