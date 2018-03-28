bfa9fba4edad8cc12e3a35c473dbdba5afa01a4f
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-28 17:12:46 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,7 +3,7 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   def self.table_name
-    "#{self.to_s.downcase}s"
+    self.to_s.downcase.pluralize
   end
 
   def self.column_names
@@ -23,39 +23,5 @@ class InteractiveRecord
     options.each do |property, value|
       self.send("#{property}=", value)
     end
-  end
-
-  def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-    DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-  end
-
-  def table_name_for_insert
-    self.class.table_name
-  end
-
-  def values_for_insert
-    values = []
-    self.class.column_names.each do |col_name|
-      values << "'#{send(col_name)}'" unless send(col_name).nil?
-    end
-    values.join(", ")
-  end
-
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  end
-
-  def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
-    DB[:conn].execute(sql)
-  end
-
-  def self.find_by(attribute_hash)
-    value = attribute_hash.values.first
-    formatted_value = value.class == Fixnum ? value : "'#{value}'"
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
-    DB[:conn].execute(sql)
   end
 end
